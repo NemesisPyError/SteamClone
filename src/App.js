3217091e41fc925e625c1273a86a3bbe8c5ca43b
NemesisPyError <titojrrfischer@gmail.com>
2025-06-25 20:52:40 -0300

    Inicio de la pagina

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import First from './componentes/firsttest'; //agregar otro archivo
+
+function App() {
+  return (
+    <div className="App">
+    <First />      
+
+    </div>
+  );
+}
+
+export default App;
